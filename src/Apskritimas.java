4a26788d74d8ca16178da0f89b1ef8ee19c0f1d4
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 15:23:50 +0300

    Konstruktoriai, getteriai-setteriai, toString

--- src/Apskritimas.java
+++ src/Apskritimas.java
@@ -1,2 +1,25 @@
 public class Apskritimas {
+    private double spindulys;
+
+    public Apskritimas(double spindulys) {
+        this.spindulys = spindulys;
+    }
+
+    public Apskritimas() {
+    }
+
+    public double getSpindulys() {
+        return spindulys;
+    }
+
+    public void setSpindulys(double spindulys) {
+        this.spindulys = spindulys;
+    }
+
+    @Override
+    public String toString() {
+        return "Apskritimas{" +
+                "spindulys=" + spindulys +
+                '}';
+    }
 }
